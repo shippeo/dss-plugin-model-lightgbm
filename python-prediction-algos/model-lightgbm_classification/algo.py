f84e9555c4df91c786ab93acdd7bc665447ba8d9
Simon Keith <simon.keith@shippeo.com>
2021-06-15 18:31:38 +0200

    blacked py files

--- python-prediction-algos/model-lightgbm_classification/algo.py
+++ python-prediction-algos/model-lightgbm_classification/algo.py
@@ -1,12 +1,14 @@
-from dataiku.doctor.plugins.custom_prediction_algorithm import BaseCustomPredictionAlgorithm
+from dataiku.doctor.plugins.custom_prediction_algorithm import (
+    BaseCustomPredictionAlgorithm,
+)
 from modellightgbm import DkuLGBMClassifier
-    
-class CustomPredictionAlgorithm(BaseCustomPredictionAlgorithm):    
-    
-    def __init__(self, prediction_type=None, params=None):      
+
+
+class CustomPredictionAlgorithm(BaseCustomPredictionAlgorithm):
+    def __init__(self, prediction_type=None, params=None):
         self.params = params
         self.clf = DkuLGBMClassifier(**self.params)
         super(CustomPredictionAlgorithm, self).__init__(prediction_type, self.params)
-        
+
     def get_clf(self):
         return self.clf
